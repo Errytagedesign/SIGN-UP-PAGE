a6ddbd3afc5b54bc1e500875b277931a19e7a3e1
Errytagedesign <errytage@gmail.com>
2021-06-21 22:51:56 +0100

    final

--- assets/js/sign-up.js
+++ assets/js/sign-up.js
@@ -4,9 +4,30 @@ const firstName = document.getElementById("firstname");
 const lastName = document.getElementById("lastname");
 const PhoneNumber = document.getElementById("number");
 const email = document.getElementById("email");
-const createPassword = document.getElementById("password1");
+const createPassword = document.getElementById("#password1");
 const confirmPassword = document.getElementById("password2");
 
+
+
+
+// Password reveal
+
+let eye = document.querySelector('.toggle-pass');
+let pass = document.querySelector('#password1');
+
+eye.addEventListener('click', () => {
+    
+    if (eye.classList.contains('bi-eye-slash')) {
+      eye.classList.remove('bi-eye-slash');
+      eye.classList.add('bi-eye');
+      pass.type = 'text';
+    } else if (eye.classList.contains('bi-eye')) {
+      pass.type = 'password';
+      eye.classList.remove('bi-eye');
+      eye.classList.add('bi-eye-slash');
+    }
+  });
+  
 
 
 // clear default action of submit form
